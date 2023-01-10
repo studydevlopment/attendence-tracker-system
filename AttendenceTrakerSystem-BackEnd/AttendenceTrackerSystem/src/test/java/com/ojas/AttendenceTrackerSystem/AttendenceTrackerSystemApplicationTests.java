8ea7a0bbf7a362be172247a09e101cf8c3b70d78
PrinceVijayy <kondavijayy123@gmail.com>
2023-01-10 19:36:09 +0530

    structure created

---
+++ AttendenceTrakerSystem-BackEnd/AttendenceTrackerSystem/src/test/java/com/ojas/AttendenceTrackerSystem/AttendenceTrackerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ojas.AttendenceTrackerSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendenceTrackerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
